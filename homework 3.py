9dcbb9986c578c31aca7e2bd702b442f566889bf
Mwungura Mugabe <mwunguramugabe@Mwunguras-MacBook-Air.local>
2025-10-14 23:14:51 -0700

    homework 3 solutions/1st task

---
+++ homework 3.py
@@ -0,0 +1,24 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.chrome.service import Service
+from webdriver_manager.chrome import ChromeDriverManager
+from time import sleep
+
+# get the path to the ChromeDriver executable
+driver_path = ChromeDriverManager().install()
+
+# create a new Chrome browser instance
+service = Service(driver_path)
+driver = webdriver.Chrome(service=service)
+driver.maximize_window()
+
+driver.get('https://stackoverflow.com/users/signup')
+driver.find_element(By.XPATH, "//h1[text()='Create your account']")
+driver.find_element(By.XPATH,"//div[@class='flex--item js-terms fs-caption fc-black-400 ta-left']")
+driver.find_element(By.ID,"email")
+driver.find_element(By.ID,"password")
+driver.find_element(By.ID,"submit-button")
+driver.find_element(By.XPATH,"//button[@data-provider='google']")
+driver.find_element(By.XPATH,"//button[@data-provider='github']")
+driver.find_element(By.XPATH,"//svg[@aria-hidden='true']")
+
